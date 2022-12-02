5778d3f81c60eb9caf54706039854dc7199c8f8d
fernandosilvam10 <fernando.silva.fsm10@gmail.com>
2022-12-02 08:51:44 -0600

    Problemas en la compilación prob 1, a y b.

--- Tarea 3_Problema_1_b).py
+++ Tarea 3_Problema_1_b).py
@@ -7,7 +7,6 @@ from tensorflow.keras.optimizers import RMSprop, Adam
 from matplotlib import pyplot as plt
 import numpy as np
 
-pi = 3.1416
 
 class ODEsolver(Sequential):
     def _init_(self,**kwargs):
@@ -23,7 +22,7 @@ class ODEsolver(Sequential):
         x = tf.random.uniform((batch_size, 1), minval=-1, maxval=1)
         with tf.GradientTape() as tape:
             y_pred = self(x, training = True)
-            eq = 3*tf.math.sin(x*pi) - y_pred
+            eq = 1+2*x+4*tf.math.pow(x,3) - y_pred
             loss = keras.losses.mean_squared_error (0,eq)
         grads = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads,self.trainable_variables))
@@ -41,19 +40,13 @@ model.add(Dense(1, activation='linear'))
 
 model.summary()
 
-model.compile(optimizer=RMSprop(), metrics=["loss"])
+model.compile(optimizer=RMSprop(), metrics=['loss'])
 x = tf.linspace(-1,1,100)
 history = model.fit(x,epochs=500, verbose = 1)
 
 x_testv = tf.linspace(-1,1,100)
 a = model.predict(x_testv)
 plt.plot(x_testv, a)
-plt.plot(x_testv, 3*np.sin(x*3.1416))
+plt.plot(x_testv, 1+2*x+4*tf.math.pow(x,3))
 
 plt.show()
-
-
-
-
-
-
